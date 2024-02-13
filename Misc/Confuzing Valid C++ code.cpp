9019358422bd325d150385ac0eb652d0b64c303d
Hexxy-Dev <Hexxy_Dev@outlook.com>
2024-02-13 19:39:25 +0200

    Original - Digraphs & Trigraphs

---
+++ Misc/Confuzing Valid C++ code.cpp
@@ -0,0 +1,23 @@
+%:include <iostream>
+
+int main()
+<%
+    uint64_t HMM = 1;
+    //??/
+    HI there...       ??/
+    HMM = 0;
+
+    if(HMM ??!??! 0) // ??/
+        std::cout << "HMMM";
+    std::cout << 1??("HMM"??);
+    std::cout << "\n";
+%>
+
+int Mainn()
+{
+    long long HMM=1;
+
+    if(HMM||0)
+        std::cout<<"HMM"[1];
+    std::cout<<"\n";
+}
