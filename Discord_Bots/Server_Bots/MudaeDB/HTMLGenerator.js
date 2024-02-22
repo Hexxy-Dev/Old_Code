abc3650a6e73420a350176167a81a10db93d7679
Hexxy-Dev <Hexxy_Dev@outlook.com>
2024-02-23 00:21:48 +0200

    Old - Discord Bots 3

--- Discord_Bots/Server_Bots/MudaeDB/HTMLGenerator.js
+++ Discord_Bots/Server_Bots/MudaeDB/HTMLGenerator.js
@@ -1,11 +1,7 @@
 /*
 ability to sort harem from the web interface
 ability to generate sort command for mudae
-add options for how many characters to show on screen at a time
 ability to generate divorce command
-
-
-search a series in multiple jsons
 */
 
 const SortTypes = [
@@ -19,13 +15,13 @@ const SortTypes = [
 
 const fs = require('fs');
 
-Start()
+Start();
 
 async function Start(){
     var UserList = [ // REDACTED
     ];
     
-    globalPath = './lists/Global/Global';
+    globalPath = './MudaeDB/lists/Global/Global';
     separator = '';
 
     if(fs.existsSync(`${globalPath}.json`)){
@@ -39,18 +35,18 @@ async function Start(){
 
 async function GenerateHTML(UserList){
     username = UserList[0];
-    userPath = './lists/' + username + '/' + username;
+    userPath = '/lists/' + username + '/' + username;
     console.log(userPath);
-    if(fs.existsSync(userPath+'.json')){
+    if(fs.existsSync('./MudaeDB'+userPath+'.json')){
         for(j=0;j<=5;j++){
-            var User = require(userPath+'.json');
+            var User = require('.'+userPath+'.json');
             console.log('found: ' + username);  
             User.sort(GetSortOrder(SortTypes[j]));
             await createHtml(User,username,userPath,SortTypes[j]);
         }
     }
     console.log('Created!');
-    if(Object.keys(UserList).length>0){
+    if(Object.keys(UserList).length>1){
         UserList.shift();
         GenerateHTML(UserList);
     }
@@ -60,8 +56,8 @@ async function GenerateHTML(UserList){
 }
 
 async function createHtml(jsonInfo,author,path,SortType){
-    if(fs.existsSync(path+`${SortType}`+'.html')){
-        fs.unlinkSync(path+`${SortType}`+'.html');
+    if(fs.existsSync('./MudaeDB'+path+`${SortType}`+'.html')){
+        fs.unlinkSync('./MudaeDB'+path+`${SortType}`+'.html');
     }
     const fsPromises = fs.promises;
     for(i=0;i<Object.keys(jsonInfo).length;i++){
@@ -71,7 +67,7 @@ async function createHtml(jsonInfo,author,path,SortType){
         else{
             info = '\n\t<h1>#' + jsonInfo[i].Order + '</h1><br>\n\t' + '<h1>' + jsonInfo[i].Name + '</h1><br>\n\t<h2>' + jsonInfo[i].Series + '</h2><br>\n\t' + '<h3>Kakera: ' + jsonInfo[i].Kakera + '</h3><br>\n\t<h3>Claim Rank: ' + jsonInfo[i].ClaimRank + '</h3><br>\n\t<h3>Like Rank: ' + jsonInfo[i].LikeRank + '</h3><br>\n\t<h3>' + jsonInfo[i].Notes + '</h3><br><br>\n\t' + `<img src="` + jsonInfo[i].ImageLink + `">`;
         }
-        await fsPromises.appendFile(`lists/${author}/${author}${SortType}.html`,`\n<div class="CharacterInfo" id="${jsonInfo[i].Name}">` + info + '\n</div>\n');
+        await fsPromises.appendFile(`./MudaeDB/lists/${author}/${author}${SortType}.html`,`\n<div class="CharacterInfo" id="${jsonInfo[i].Name}">` + info + '\n</div>\n');
     }
     return true;
 }
@@ -112,13 +108,13 @@ async function writeJsonStart(globalPath,UserList,separator){
 
 async function createGlobalJSON(userList,separator,globalPath){
     username = userList[0];
-    userPath = './lists/' + username + '/' + username + '.json';
-    if(fs.existsSync(userPath)){
-        jsonUserInfo = require(userPath);
+    userPath = '/lists/' + username + '/' + username + '.json';
+    if(fs.existsSync('./MudaeDB'+userPath)){
+        jsonUserInfo = require('.'+userPath);
 
         for(k=0;k<Object.keys(jsonUserInfo).length;k++){
             jsonWriteInfo = separator + `{"BelongsTo":"${username}","Name":"${jsonUserInfo[k].Name}","Series":"${jsonUserInfo[k].Series}","Kakera":${jsonUserInfo[k].Kakera},"ClaimRank":${jsonUserInfo[k].ClaimRank},"LikeRank":${jsonUserInfo[k].LikeRank},"Notes":"${jsonUserInfo[k].Notes}","ImageLink":"${jsonUserInfo[k].ImageLink}"}`;
-            fs.appendFile(`${globalPath}.json`,jsonWriteInfo, function (err) {
+            fs.appendFileSync(`${globalPath}.json`,jsonWriteInfo, function (err) {
                 if (err) throw err;
                 //console.log(`Updated json for Global`);
             });
@@ -126,6 +122,7 @@ async function createGlobalJSON(userList,separator,globalPath){
                 separator = ',\n';
             }
         }
+
     }
     console.log('Created!');
     if(Object.keys(userList).length>0){
@@ -154,4 +151,3 @@ async function writeJsonEnd(globalPath){
         console.log('All tasks completed!!')
     }, 1000);
 }
-
